f7a283e0f6f2596abd1fcc149abd1096eb0d2542
George-1999-os <georgeibuchi44@gmail.com>
2025-09-12 12:35:39 +0100

    Clean project structure and fix tests for ALX checker

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,4 +1,15 @@
-from django.http import HttpResponse
+from rest_framework import viewsets
+from .models import Listing, Booking, Review
+from .serializers import ListingSerializer, BookingSerializer, ReviewSerializer
 
-def dummy_view(request):
-    return HttpResponse("Hello")
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
